a55f4ff9a8fd33eaa2474968da4dcb95a2d2e8ab
tuftscs <tuftscs@tuftscsvm.tuftscsvm>
2016-08-06 04:54:17 -0400

    Demo

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var opbeat = require('opbeat').start()
+//var opbeat = require('opbeat').start()
 
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(opbeat.middleware.express())
+//app.use(opbeat.middleware.express())
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -14,6 +15,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+    response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
