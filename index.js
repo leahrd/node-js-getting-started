3684f3e2ee0bd1a6f11844ca89af2c72f9ba5718
leahrd <leahdanny1991@yahoo.com>
2016-08-06 07:45:16 -0400

    Am I doing this right 3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/lab8', express.static(__dirname + '/public'));
 //app.use(express.static(__dirname + '/public'));
 //app.use(opbeat.middleware.express())
 
