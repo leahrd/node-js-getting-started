1ca5a458e89cc71e5cd368970f7a14c88cde9225
leahrd <leahdanny1991@yahoo.com>
2016-08-06 08:07:47 -0400

    Am I doing this right 10

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/lab8', express.static(__dirname + '/public'));
 //app.use(express.static(__dirname + '/public'));
 //app.use(opbeat.middleware.express())
 
@@ -25,7 +25,6 @@ app.set('view engine', 'ejs');
 
 app.get('/lab8', function(request,response) {
     response.send(__dirname + 'public/mystatic.html');
-    //response.sendfile('public/mystatic.html');
 });
 
 app.listen(app.get('port'), function() {
