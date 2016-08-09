31ca8692d69be9da85e8c33f943431f7aa56d85c
leahrd <leahdanny1991@yahoo.com>
2016-08-09 15:35:14 -0400

    Not working but need to push

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 
 var express = require('express');
 
-//var bodyParser = require('body-parser');
+
 var cors = require('cors');
 var app = express();
 var needle = require('needle');
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.use('/',cors());
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
+//app.use(cors());
+//views is directory for all template files
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/redline.json', function(request, response) {
-    //response.sendfile('public/mystatic.html');
-    response.set('Content-Type','text/plain');
+    response.set('Content-Type', 'text/plain');
     var data = "GET didn't work";
     needle.get('http://developer.mbta.com/lib/rthr/red.json', function(error, response) {
 	if(!error && response.statusCode == 200) {
-	    data = response.body;
-
+	    data = response.body;	    
 	    response.send(data);
+	    
 	}
 	else {
 	    data = "ERROR: " + error + "status is: " + response.statusCode;
