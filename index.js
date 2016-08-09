7767997ce21808a270484aa31bb3c963309cedce
leahrd <leahdanny1991@yahoo.com>
2016-08-09 16:59:54 -0400

    Having trouble 1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
+var cool = require('cool-ascii-faces');
 var express = require('express');
 
 
-var cors = require('cors');
+//var cors = require('cors');
 var app = express();
 var needle = require('needle');
 
@@ -15,17 +15,34 @@ app.use(express.static(__dirname + '/public'));
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/', function(request,response) {
+    response.render('pages/index');
+});
+
+app.get('/cool', function(request,response) {
+    response.send(cool());
+});
+
+app.get('/lab8', function(request,response) {
+    response.sendFile(__dirname + '/public/mystatic.html');
+});
+
+
+
 app.get('/redline.json', function(request, response) {
-    response.header('Access-Control-Allow-Origin', 'http://developer.mbta.com/lib/rthr/red.json');
-    response.header('Access-Control-Allow-Methods', 'GET, POST');
-    response.header('Access-Control-Allow-Methods', 'Content-Type', 'text/plain');
-    response.set('Content-Type', 'text/plain');
-    var data = "GET didn't work";
-    needle.get('http://developer.mbta.com/lib/rthr/red.json', function(error, response) {
+    response.header("Access-Control-Allow-Origin", "*");
+    //response.header('Access-Control-Allow-Methods', 'GET, POST');
+    //response.header('Access-Control-Allow-Methods', 'X-Requested-With, Content-Type, Accept');
+    response.header("Access-Control-Allow-Methods", "X-Requested-With");
+    //response.set('Content-Type','text/plain');
+    var data = " ";
+    
+    needle.get('http://developer.mbta.com/lib/rthr/red.json', function(error, resp) {
 	if(!error && response.statusCode == 200) {
 
-	    data = response.body;	    
-	    response.send(data);
+	    data = resp.body;	    
+	    resp.set('Content-Type', 'text/plain');
+	    resp.send(data);
 	    
 	}
 	else {
